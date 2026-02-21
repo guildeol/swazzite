9c9d88fdc690cbea0c43edcc746317d53bab0266
Guilherme Costa <glhrmcosta91@gmail.com>
2026-02-21 18:16:22 +0000

    Add sway packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install Sway packages
+dnf5 install -y sway swaybg swaylock swayidle waybar grim
 
 # Use a COPR Example:
 #
